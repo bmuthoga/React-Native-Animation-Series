f8c5d6fcb81a1e438db0d723e3b94c8a9769efff
Batian Muthoga <bmuthoga@gmail.com>
2020-06-04 22:09:58 +0300

    feat(AirbnbClone): implement header animation

---
+++ AirBnBClone/src/api/images.js
@@ -0,0 +1,6 @@
+export const images = [
+  {id: 1, uri: require('../assets/img/1_.jpg')},
+  {id: 2, uri: require('../assets/img/2_.jpg')},
+  {id: 4, uri: require('../assets/img/4_.jpg')},
+  {id: 9, uri: require('../assets/img/9_.jpg')},
+];
